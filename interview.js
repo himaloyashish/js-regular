7b96cf2e445955d016c3b1747341d2bd96028e84
himaloyash <himaloyashis98@gmail.com>
2023-12-30 10:34:15 +0600

    longest word problem solved

--- interview.js
+++ interview.js
@@ -12,8 +12,43 @@ let part = text.substr(7, 6);
 const sentence = " Ashish is a good boy with passion focused self employed man. He is named Himaloy by mam and Ashu by grand ma Ashish by baba and friends and bala by friends and brothers sometimes. "
 
 const matches = sentence.match(/Ashish/gi);
-console.log(matches.length);
+// console.log(matches.length);
 
 let position = sentence.search(/Ashish/i)
 position = position >= 0 ? position : "Not found";
-console.log(position);
+// console.log(position);
+
+
+function linear(arr, value) {
+    const length = arr.length
+
+    for (let i = 0; i< length; i++ ){
+        if(arr[i] === value){
+            return i;
+        }
+    }
+   
+
+    return "not found";
+}
+
+console.log(linear(['a', 'b', 'c', 'd'], 'd'));
+
+// problem longest number
+function getLongest(names) {
+
+    let longestWord = ""
+
+    for (name of names){
+        if(name.length > longestWord){
+            longestWord = name
+            
+        }
+
+    }
+
+    return [longestWord, names.indexOf(longestWord)]
+}
+
+// console.log(getLongest(["Himaloy Bala Ashish", "Bithy angle", "Ashish007"]));
+
