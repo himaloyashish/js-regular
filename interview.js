2f73c7a0965e07a508202328d15c73dbac4065bc
himaloyash <himaloyashis98@gmail.com>
2023-12-31 23:48:31 +0600

    function is called itself

--- interview.js
+++ interview.js
@@ -39,7 +39,7 @@ function getLongest(names) {
 
     let longestWord = ""
 
-    for (name of names) {
+    for (let name of names) {
         if (name.length > longestWord) {
             longestWord = name
 
@@ -135,11 +135,58 @@ function Person(first, last, age) {
     this.lastName = last;
     this.ages = age;
     this.fullName = function () {
-        
+
         return this.firstName + " " + this.lastName
     }
 }
 
 const Ashish = new Person("Himaloy", "Ashish", 26)
 
-console.log(Ashish.fullName());
+// console.log(Ashish.fullName());
+
+
+// iterator in js
+
+const nums = [1, 2, 3]
+
+const iterators = nums[Symbol.iterator]()
+
+// console.log(iterators.next());
+
+const iterateObject = {a:'a', b:'b'}
+
+// console.dir(iterateObject);
+
+iterateObject[Symbol.iterator] = function () {
+
+    let n = 0;
+    let done = false;
+    return {
+        next() {
+            n += 10;
+            if (n == 100) { done = true }
+            return { value: n, done: done }
+        }
+    }
+}
+
+for(naam of iterateObject){
+    // console.log(naam);
+}
+
+const letters = new Set(['a']);
+
+// Add Values to the Set
+
+const fruits = new Map([['banana', 500]])
+// console.log(fruits);
+
+const foll = new Map()
+
+foll.set('Mango', 50)
+foll.set('Apple', 50)
+foll.set('Jam', 50)
+
+// console.log(fol);
+
+
