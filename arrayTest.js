564b9406a0812abfb749f40379ab93b308ced7d7
himaloyash <himaloyashis98@gmail.com>
2024-01-01 15:30:46 +0600

    duplicate is on a action

--- arrayTest.js
+++ arrayTest.js
@@ -1,3 +1,4 @@
+//  check array1 elements with array2 elements
 const array1 = [8, 9,10, 4, 5];
 
 const array2 = [9, 10, 4, 5, 8]
@@ -5,10 +6,28 @@ const array2 = [9, 10, 4, 5, 8]
 
 const arrayChecker = array1.length == array2.length && array1.every((currentValue)=>{
     if(array2.indexOf(currentValue)> -1){
-        console.log(currentValue ,array2[array2.indexOf(currentValue)]);
+        // console.log(currentValue ,array2[array2.indexOf(currentValue)]);
         
         return ( currentValue = array2[array2.indexOf(currentValue)] )
     }
 })
 
-console.log(arrayChecker);
+// console.log(arrayChecker);
+
+const duplicate = [1, 34, 5, 4, 5, 45, 5, 3, 4, 5, 3, 2, 6, 4, 6, 3, 7, 8, 2, 6, 9 , 5, ]
+
+const authenticArray =  [...new Set(duplicate)];
+
+const duplicator = [1, 34, 5, 4, 5, 45, 5, 3, 4, 5, 3, 2, 6, 4, 6, 3, 7, 8, 2, 6, 9 , 5, ]
+
+const authi = duplicator.filter ( (value, index, array)=>{
+
+    return array.indexOf(value) !== index
+    
+    
+})
+
+console.log(authi);
+
+   
+
