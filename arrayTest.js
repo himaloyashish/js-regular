fcd8a76fcb34c8f80b8e9ea2b52f28e379f76087
himaloyash <himaloyashis98@gmail.com>
2024-01-01 12:41:59 +0600

    arrayTest is finally done

--- arrayTest.js
+++ arrayTest.js
@@ -3,6 +3,12 @@ const array1 = [8, 9,10, 4, 5];
 const array2 = [9, 10, 4, 5, 8]
 
 
-const arrayChecker = array1.length === array2.length
+const arrayChecker = array1.length == array2.length && array1.every((currentValue)=>{
+    if(array2.indexOf(currentValue)> -1){
+        console.log(currentValue ,array2[array2.indexOf(currentValue)]);
+        
+        return ( currentValue = array2[array2.indexOf(currentValue)] )
+    }
+})
 
 console.log(arrayChecker);
