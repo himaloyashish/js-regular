2cd79c1f4e86d7f74a17bc4ed1ad2414dd831008
himaloyash <himaloyashis98@gmail.com>
2024-01-01 12:23:24 +0600

    problem solving one

---
+++ arrayTest.js
@@ -0,0 +1,8 @@
+const array1 = [8, 9,10, 4, 5];
+
+const array2 = [9, 10, 4, 5, 8]
+
+
+const arrayChecker = array1.length === array2.length
+
+console.log(arrayChecker);
--- function.js
+++ function.js
@@ -1,3 +1,21 @@
-(function (){
+(function () {
     // console.log("object");
 })()
+
+x = findMax(12, 332, 500, 53)
+
+function findMax() {
+    let max = -Infinity;
+
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] > max) {
+            max = arguments[i]
+        }
+
+    }
+
+    return max
+}
+
+// console.log(x);
+
