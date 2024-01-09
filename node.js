33bca5194fc2b7585414c20d4163968a388c6cdc
himaloyash <himaloyashis98@gmail.com>
2024-01-10 00:34:06 +0600

    node is done

---
+++ node.js
@@ -0,0 +1,9 @@
+const deep = document.getElementById('deep');
+const node = document.createElement('p')
+node.innerHTML= "I'm a new p tag";
+
+const p2 = document.getElementById('p2')
+const p1 = document.getElementById('p1')
+
+deep.insertBefore(node, p2)
+// deep.removeChild(p1)
