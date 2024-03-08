12b797fcf382befeae28ac616fbd39754d0393e8
himaloyash <himaloyashis98@gmail.com>
2024-03-09 00:10:10 +0600

    time in enough time

--- time.js
+++ time.js
@@ -1,3 +1,28 @@
-const interval = document.getElementById("interval");
 const timeOut = document.getElementById("timeOut");
+const interval = document.getElementById("interval");
 
+let timeInstance;
+let intervalInstance;
+
+function startTimeOut() {
+    timeInstance = setTimeout(function () {
+        timeOut.innerHTML = "3 seconds is over "
+    }, 3000);
+
+}
+
+function stopTimeOut() {
+    clearTimeout(timeInstance)
+}
+
+
+function startInterval() {
+    intervalInstance = setInterval(function () {
+        interval.innerHTML = new Date().toLocaleDateString()
+    }, 3000);
+}
+
+
+function stopInterval() {
+    clearInterval(intervalInstance)
+}
