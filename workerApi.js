8c4fa1edc875688a2d2492fcbc9cd19aac756c30
himaloyash <himaloyashis98@gmail.com>
2024-03-10 10:24:04 +0600

    all about ok on worker

--- workerApi.js
+++ workerApi.js
@@ -1,9 +1,30 @@
-const worker = ''
+let w;
 
-let i = 0;
+function startWorker() {
 
-while ( i < 100000000) {
-    i++;
+    if (typeof Worker !== "undefined") {
+        // worker available here;
+
+        // if the worker does not work
+        if (w == "undefined") {
+            w = new Worker("hang.js");
+
+        }
+
+        w.onmessage = function (event) {
+            document.getElementById("demo").innerHTML = event.data
+        }
+
+    } else {
+        alert("worker is unsupported in this browser");
+    }
 }
 
-postMessage(i);
+function stopWorker() {
+    if (typeof Worker !== "undefined") {
+        w.terminate();
+        w = undefined;
+    } else {
+        alert("worker is unsupported in this browser");
+    }
+}
