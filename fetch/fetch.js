4ba03eb2ddd843e112b7cc9d19606f2ebb677863
himaloyash <himaloyashis98@gmail.com>
2024-03-17 16:06:27 +0600

    api almost done

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -5,7 +5,8 @@ const fetches = document.getElementById('fetch')
 async function getData(){
 
     const res = await fetch('http://localhost:5500/fetch/json.txt');
-    const data = await res.text();
+    console.log(res);
+    const data = await res.json()
 
     fetches.innerHTML = data
 
