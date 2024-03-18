57cc875bf07095ea5fb9185d2f8ddd75b616c980
himaloyash <himaloyashis98@gmail.com>
2024-03-18 23:30:42 +0600

    not ok req

--- ajaxhttpreq/xmlhttpreq.js
+++ ajaxhttpreq/xmlhttpreq.js
@@ -1,17 +1,18 @@
-const div = document.getElementById();
 
 function loadData () {
-
+    
     // create a new request
     const xhr = new XMLHttpRequest();
-
+    
     // what to do when response arrives
-
+    
     xhr.onload = function () {
-        
+        const div = document.getElementById("demo");
+        demo.innerHtml = xhr.responseText;
+
     }
 
-
-
+    xhr.open("GET", "./data.txt");
+    xhr.send();
 
 }
