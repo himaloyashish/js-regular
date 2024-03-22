70ea4b14b7230bc667693c1163ce3125aed9d1bb
himaloyash <himaloyashis98@gmail.com>
2024-03-22 16:00:04 +0600

    new data fetch is on ajax

--- ajaxhttpreq-lesson3/xmlhttpreq.js
+++ ajaxhttpreq-lesson3/xmlhttpreq.js
@@ -1,13 +1,8 @@
-function loadData (){
+function getData (){
 
-    const xhr= new XMLHttpRequest()
-
-    xhr.onload = function (){
-        const container = document.getElementById("demo");
-        demo.innerHTML = xhr.responseText;
-    }
-
-    xhr.open("GET", "./data.txt");
-    xhr.send();
- 
 }
+
+function sendData(){
+
+}
+
