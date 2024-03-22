fd8f360465d8b25452adc09916fdadab574cccb9
himaloyash <himaloyashis98@gmail.com>
2024-03-22 22:31:35 +0600

    req send and res comes

--- ajaxhttpreq-lesson3/xmlhttpreq.js
+++ ajaxhttpreq-lesson3/xmlhttpreq.js
@@ -1,8 +1,22 @@
-function getData (){
+function getData() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+        console.log(xhr.responseText);
+    }
+
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
+
+    xhr.send();
+}
+
+function sendData() {
 
 }
 
-function sendData(){
+const sendButton = document.getElementById("send");
+const getButton = document.getElementById("get");
 
-}
 
+sendButton.addEventListener("click", sendData);
+getButton.addEventListener("click", getData);
