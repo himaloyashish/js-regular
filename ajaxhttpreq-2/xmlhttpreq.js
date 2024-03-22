cc910dec2dda9d38af248b161cda2526f2102d6d
himaloyash <himaloyashis98@gmail.com>
2024-03-22 12:59:06 +0600

    now the xhr is on handled by  callback function

--- ajaxhttpreq-2/xmlhttpreq.js
+++ ajaxhttpreq-2/xmlhttpreq.js
@@ -1,13 +1,26 @@
-function loadData (){
+function loadData(callbackFunction) {
 
-    const xhr= new XMLHttpRequest()
+    const xhr = new XMLHttpRequest()
 
-    xhr.onload = function (){
-        const container = document.getElementById("demo");
-        demo.innerHTML = xhr.responseText;
+    xhr.onload = function () {
+        callbackFunction(this);
     }
 
     xhr.open("GET", "./data.txt");
+
+    xhr.setRequestHeader("My_gf", "Mitu");
+
+
     xhr.send();
- 
+
+}
+
+function callbackFunction1(xhr) {
+    const container = document.getElementById("demo");
+    container.innerHTML = xhr.responseText;
+}
+
+function callbackFunction2(xhr) {
+    const container = document.getElementById("demo2");
+    container.innerHTML = xhr.responseText;
 }
